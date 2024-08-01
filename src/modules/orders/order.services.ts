ee970dc02867958c06ef664ac893fc00fc0e8f7e
farhanAfrad <farhanafrad796@gmail.com>
2024-08-02 02:02:22 +0600

    when order is made product quantity will be adjusted and if product quantity reaches to 0 then inStock property will be changed to false

--- src/modules/orders/order.services.ts
+++ src/modules/orders/order.services.ts
@@ -1,22 +1,32 @@
-import { TProduct } from "../products/product.interface";
+
 import { Product } from "../products/product.model";
 import { TOrder } from "./order.interface";
 import { Order } from "./order.model";
 
 const createOrderIntoDB = async (orderData: TOrder) => {
+    // 1stly productId from order data is retrieved
     const productData = await Product.findById(orderData.productId);
-    // console.log(productData.inventory.quantity);
+    // as ordered product could not be in the database hence the value can be null that's why it is added in the condition then also checked product quantity is available
     if (productData !== null && productData.inventory.quantity > 0) {
+
+        // if ordered quantity is larger than the available product quantity than an message will send to the customer
         if (orderData.quantity > productData.inventory.quantity) {
             throw new Error("Insufficient quantity available in inventory")
         }
         const result = await Order.create(orderData);
-        // const productId = result.productId;
 
+        // here calculation is done after order how much quantiy is available
         const newQuantity = productData.inventory.quantity - orderData.quantity;
         // console.log(newQuantity);
-        await Product.updateOne({ _id: result.productId }, { "inventory.quantity": newQuantity })
+        const modifiedProduct = await Product.findByIdAndUpdate({ _id: result.productId }, { "inventory.quantity": newQuantity }, { new: true })
+        // when product quantity reaches 0 inStock will change to false
+        if (modifiedProduct?.inventory.quantity === 0) {
+            await Product.findByIdAndUpdate({ _id: result.productId }, { "inventory.inStock": false }, { new: true })
+        }
         return result;
+    }
+    else {
+        throw new Error("Insufficient quantity available in inventory")
     }
 }
 
